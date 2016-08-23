14350f019842f1d52ac2891f97caad38f913dfa0
Richard Wu <richardwu1997@gmail.com>
2016-08-23 02:12:40 -0400

    added scrape pattern yml file and root domain regex

--- lib/news_scraper/extractors/article.rb
+++ lib/news_scraper/extractors/article.rb
@@ -1,9 +1,14 @@
+require 'nokogiri'
+require 'yaml'
+
 module NewsScraper
   module Extractors
     class Article
       include ExtractorsHelpers
 
       attr_accessor :uri
+
+      SCRAPE_PATTERNS = YAML.load_file('config/article_scrape_patterns.yml').freeze
 
       def initialize(uri:)
         @uri = uri.gsub(/^https?:\/\//, '')
@@ -13,6 +18,12 @@ module NewsScraper
         http_request "http://#{uri}" do |response|
           response.body
         end
+      end
+
+      private
+
+      def root_domain
+        @root_domain ||= uri.downcase.match(/^(?:[\w\d-]+\.)?([\w\d-]+\.\w{2,})/)[1]
       end
     end
   end
