f5525c69cd1398660f57e50c8485c2444ab106ea
Julian Nadeau <julian@jnadeau.ca>
2016-09-24 09:52:49 -0400

    Add tests for error handling

--- test/unit/scraper_test.rb
+++ test/unit/scraper_test.rb
@@ -16,16 +16,16 @@ module NewsScraper
         title: 'Shopify is Great',
         root_domain: 'somearticle.com'
       }
-      NewsScraper::Transformers::Article.any_instance.expects(:transform).returns(@transformed_data)
-
       @scraper = NewsScraper::Scraper.new(query: 'shopify')
     end
 
     def test_scrape_returns_an_array_of_data_if_no_block_is_given
+      NewsScraper::Transformers::Article.any_instance.expects(:transform).returns(@transformed_data)
       assert_equal [@transformed_data], @scraper.scrape
     end
 
     def test_scrape_yields_each_transformed_article_if_block_is_given
+      NewsScraper::Transformers::Article.any_instance.expects(:transform).returns(@transformed_data)
       yielded_articles = []
       @scraper.scrape do |transformed_article|
         yielded_articles << transformed_article
@@ -36,7 +36,27 @@ module NewsScraper
     end
 
     def test_scrape_also_returns_array_of_transformed_articles_if_block_is_given
+      NewsScraper::Transformers::Article.any_instance.expects(:transform).returns(@transformed_data)
       assert_equal [@transformed_data], @scraper.scrape { |_| }
+    end
+
+    def test_scrape_yields_errors_if_block_is_given
+      NewsScraper::Transformers::Article.any_instance.expects(:transform).raises(Transformers::ScrapePatternNotDefined)
+
+      yielded_errors = []
+      @scraper.scrape do |should_be_an_error|
+        yielded_errors << should_be_an_error
+      end
+      assert_equal 1, yielded_errors.count
+      assert_kind_of Transformers::ScrapePatternNotDefined, yielded_errors.first
+    end
+
+    def test_scrape_raises_error_if_no_block_given
+      NewsScraper::Transformers::Article.any_instance.expects(:transform).raises(Transformers::ScrapePatternNotDefined)
+
+      assert_raises Transformers::ScrapePatternNotDefined do
+        @scraper.scrape
+      end
     end
   end
 end
