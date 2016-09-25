34344fe2b1a099e63a2e8b05ac2c8da8cfa16169
Julian Nadeau <julian@jnadeau.ca>
2016-09-25 17:01:53 -0400

    Fix tests

--- test/helpers/extractors_test_helpers.rb
+++ test/helpers/extractors_test_helpers.rb
@@ -1,9 +1,10 @@
 module ExtractorsTestHelpers
   def stub_http_request(url:, body:)
-    HTTParty.expects(:get).with(url).returns(stub(
-      body: body,
-      code: 200,
-      message: 'OK'
-    ))
+    HTTParty.expects(:get).with(url, headers: { "User-Agent" => "news-scraper-#{NewsScraper::VERSION}" })
+            .returns(stub(
+              body: body,
+              code: 200,
+              message: 'OK'
+            ))
   end
 end
