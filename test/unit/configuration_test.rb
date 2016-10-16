a4fda3b73329e442149c60d3554023f5af2e0c24
Julian Nadeau <julian@jnadeau.ca>
2016-10-16 15:13:25 -0400

    Add some tests

--- test/unit/configuration_test.rb
+++ test/unit/configuration_test.rb
@@ -7,12 +7,20 @@ module NewsScraper
       assert_equal({ 'domains' => 'test' }, NewsScraper.configuration.scrape_patterns)
     end
 
+    def test_stopwords_loaded_from_stopwords_fetch_method
+      NewsScraper.configuration.stopwords_fetch_method = proc { ['banana'] }
+      assert_equal(['banana'], NewsScraper.configuration.stopwords)
+    end
+
     def test_reset_configuration_sets_default_filepath
       NewsScraper.configuration.scrape_patterns_fetch_method = proc { { 'domains' => 'test' } }
+      NewsScraper.configuration.stopwords_fetch_method = proc { ['banana'] }
       NewsScraper.reset_configuration
 
       assert_equal YAML.load_file(Configuration::DEFAULT_SCRAPE_PATTERNS_FILEPATH),
         NewsScraper.configuration.scrape_patterns
+      assert_equal YAML.load_file(Configuration::STOPWORDS_FILEPATH),
+        NewsScraper.configuration.stopwords
     end
   end
 end
